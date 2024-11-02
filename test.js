52129dec1187146072faaf825d6c7738e09de2c0
rajibmalik <rajibmalik@hotmail.co.uk>
2024-11-02 17:50:28 +0000

    Updated Script tag

---
+++ test.js
@@ -0,0 +1,15 @@
+// var myNum = 32;
+// function myFunction() {
+//   var myNum = 5;
+//   return myNum;
+//   var myNum = 10;
+//   return myNum;
+// }
+
+// console.log(myFunction());
+
+var animal = "Dog"
+  ? console.log("The animal is a dog")
+  : console.log("The animal is a cat");
+
+console.log(animal);
